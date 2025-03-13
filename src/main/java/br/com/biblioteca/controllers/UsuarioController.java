10110a8aa52461bbfb4a1130167693e9f920497c
arturmartins <arturmartinssilva078@gmail.com>
2025-03-13 12:45:05 -0300

    feat(controllers): adiciona método para buscar usuários pelo nome no UsuarioController

--- src/main/java/br/com/biblioteca/controllers/UsuarioController.java
+++ src/main/java/br/com/biblioteca/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.biblioteca.dto.UsuarioDTO;
@@ -32,6 +33,11 @@ public class UsuarioController {
     @GetMapping("/{id}")
     public UsuarioDTO buscarUsuarioPorID(@PathVariable Long id) {
         return usuarioService.buscarUsuarioPorID(id);
+    }
+
+    @GetMapping("/buscar-usuario-nome")
+    public List<UsuarioDTO> buscarUsuarioPorNome(@RequestParam String nome) {
+        return usuarioService.buscarUsuarioPorNome(nome);
     }
 
     @PostMapping
