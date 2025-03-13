e584c92cb1472f662882ee3bb6a6e2327cc652d0
arturmartins <arturmartinssilva078@gmail.com>
2025-03-12 22:34:20 -0300

    feat(controllers): adiciona métodos para buscar, atualizar e excluir usuários no UsuarioController

--- src/main/java/br/com/biblioteca/controllers/UsuarioController.java
+++ src/main/java/br/com/biblioteca/controllers/UsuarioController.java
@@ -3,21 +3,24 @@ package br.com.biblioteca.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.biblioteca.dto.UsuarioDTO;
 import br.com.biblioteca.services.UsuarioService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-    
+
     @Autowired
     private UsuarioService usuarioService;
 
@@ -26,10 +29,24 @@ public class UsuarioController {
         return usuarioService.listarTodosUsuarios();
     }
 
+    @GetMapping("/{id}")
+    public UsuarioDTO buscarUsuarioPorID(@PathVariable Long id) {
+        return usuarioService.buscarUsuarioPorID(id);
+    }
+
     @PostMapping
     public void adicionarUsuario(@RequestBody UsuarioDTO usuario) {
         usuarioService.adicionarUsuario(usuario);
     }
-    
-    
+
+    @PutMapping
+    public UsuarioDTO atualizarUsuario(@RequestBody UsuarioDTO usuario) {
+        return usuarioService.atualizarUsuario(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.ok().build();
+    }
 }
