ded6b3753f7deed0c0bf0a565e510da5b066c0f2
arturmartins <arturmartinssilva078@gmail.com>
2025-03-13 13:12:13 -0300

    feat(LivroRepository): adiciona repositório LivroRepository para gerenciamento de livros

---
+++ src/main/java/br/com/biblioteca/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package br.com.biblioteca.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.biblioteca.entities.LivroEntity;
+
+public interface LivroRepository extends JpaRepository<LivroEntity, Long> {
+    List<LivroEntity> findByNome(String nome);
+}
