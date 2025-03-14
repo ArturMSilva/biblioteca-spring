eafa22f1a4a8f80ecd730e3689228aa8da131933
arturmartins <arturmartinssilva078@gmail.com>
2025-03-14 10:45:24 -0300

    feat(EnderecoController): adiciona controlador para gerenciamento de endereços com operações CRUD

---
+++ src/main/java/br/com/biblioteca/controllers/EnderecoController.java
@@ -0,0 +1,59 @@
+package br.com.biblioteca.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.biblioteca.dto.EnderecoDTO;
+import br.com.biblioteca.services.EnderecoService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @GetMapping
+    public List<EnderecoDTO> listarTodosEnderecos() {
+        return enderecoService.bucarTodosEnderecos();
+    }
+
+    @GetMapping("/{id}")
+    public EnderecoDTO buscarEnderecoPorId(@PathVariable Long id) {
+        return enderecoService.buscarEnderecoPorId(id);
+    }
+
+    @GetMapping("/buscar-endereco-cep")
+    public List<EnderecoDTO> buscarEnderecoPorCEP(@RequestParam String cep) {
+        return enderecoService.buscarEnderecoPorCEP(cep);
+    }
+
+    @PostMapping
+    public void adicionarEndereco(@Validated @RequestBody EnderecoDTO endereco) {
+        enderecoService.adicionarEndereco(endereco);
+    }
+
+    @PutMapping
+    public EnderecoDTO atualizarEndereco(@Validated @RequestBody EnderecoDTO endereco) {
+        return enderecoService.atualizarEndereco(endereco);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarEndereco(@PathVariable Long id) {
+        enderecoService.deletarEndereco(id);
+        return ResponseEntity.ok().build();
+    }
+}
