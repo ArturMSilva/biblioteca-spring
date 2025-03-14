e7239f057fa0f4956e2a721d62ba7f1671a42e32
arturmartins <arturmartinssilva078@gmail.com>
2025-03-14 17:44:37 -0300

    feat(EmprestimoEntity): adiciona construtor que recebe EmprestimoDTO e ajusta tipo de devolvido para Boolean

--- src/main/java/br/com/biblioteca/entities/EmprestimoEntity.java
+++ src/main/java/br/com/biblioteca/entities/EmprestimoEntity.java
@@ -1,7 +1,9 @@
 package br.com.biblioteca.entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import br.com.biblioteca.dto.EmprestimoDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,12 +32,23 @@ public class EmprestimoEntity {
     private LocalDate dataDevolucao;
 
     @Column(nullable = false)
-    private boolean devolvido;
+    private Boolean devolvido;
+
+    public EmprestimoEntity(EmprestimoDTO emprestimoDTO) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.id = emprestimoDTO.id();
+        this.usuario = (emprestimoDTO.usuario() != null) ? new UsuarioEntity(emprestimoDTO.usuario()) : null;
+        this.livro = (emprestimoDTO.livro() != null) ? new LivroEntity(emprestimoDTO.livro()) : null;
+        this.dataEmprestimo = LocalDate.parse(emprestimoDTO.dataEmprestimo(), formatter);
+        this.dataDevolucao = LocalDate.parse(emprestimoDTO.dataDevolucao(), formatter);
+        this.devolvido = emprestimoDTO.devolvido();
+    }
 
     public EmprestimoEntity() {
     }
 
-    public EmprestimoEntity(Long id, UsuarioEntity usuario, LivroEntity livro, LocalDate dataEmprestimo, LocalDate dataDevolucao, boolean devolvido) {
+    public EmprestimoEntity(Long id, UsuarioEntity usuario, LivroEntity livro, LocalDate dataEmprestimo,
+            LocalDate dataDevolucao, Boolean devolvido) {
         this.id = id;
         this.usuario = usuario;
         this.livro = livro;
