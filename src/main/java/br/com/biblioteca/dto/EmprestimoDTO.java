82e5a2ae0bbf805e529981486e00ba98ef50c3f6
arturmartins <arturmartinssilva078@gmail.com>
2025-03-14 17:44:54 -0300

    feat(EmprestimoDTO): adiciona classe DTO para representar dados de empréstimos

---
+++ src/main/java/br/com/biblioteca/dto/EmprestimoDTO.java
@@ -0,0 +1,21 @@
+package br.com.biblioteca.dto;
+
+import java.time.format.DateTimeFormatter;
+
+import br.com.biblioteca.entities.EmprestimoEntity;
+
+public record EmprestimoDTO(Long id, UsuarioDTO usuario, LivroDTO livro, String dataEmprestimo, String dataDevolucao,
+        Boolean devolvido) {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public EmprestimoDTO(EmprestimoEntity emprestimoEntity) {
+        this(
+                emprestimoEntity.getId(),
+                (emprestimoEntity.getUsuario() != null) ? new UsuarioDTO(emprestimoEntity.getUsuario()) : null,
+                (emprestimoEntity.getLivro() != null) ? new LivroDTO(emprestimoEntity.getLivro()) : null,
+                emprestimoEntity.getDataEmprestimo().format(formatter),
+                emprestimoEntity.getDataDevolucao().format(formatter),
+                emprestimoEntity.getDevolvido());
+    }
+}
