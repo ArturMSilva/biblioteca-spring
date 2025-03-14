58a230a255208a81640ad9b79263d5e3d6b5a8c9
arturmartins <arturmartinssilva078@gmail.com>
2025-03-14 09:29:39 -0300

    fix(LivroDTO): altera tipos de anoPublicacao, quantidadeExemplares e emprestado para Integer e Boolean

--- src/main/java/br/com/biblioteca/dto/LivroDTO.java
+++ src/main/java/br/com/biblioteca/dto/LivroDTO.java
@@ -2,8 +2,8 @@ package br.com.biblioteca.dto;
 
 import br.com.biblioteca.entities.LivroEntity;
 
-public record LivroDTO(Long id, String titulo, String nomeAutor, int anoPublicacao, int quantidadeExemplares,
-        String genero, boolean emprestado) {
+public record LivroDTO(Long id, String titulo, String nomeAutor, Integer anoPublicacao, Integer quantidadeExemplares,
+        String genero, Boolean emprestado) {
     public LivroDTO(LivroEntity livroEntity) {
         this(
                 livroEntity.getId(),
@@ -12,6 +12,6 @@ public record LivroDTO(Long id, String titulo, String nomeAutor, int anoPublicac
                 livroEntity.getAnoPublicacao(),
                 livroEntity.getQuantidadeExemplares(),
                 livroEntity.getGenero(),
-                livroEntity.isEmprestado());
+                livroEntity.getEmprestado());
     }
 }
