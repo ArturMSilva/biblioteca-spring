aa94f0e7f636fe367b5acc7c9a9ab973bc6d4cbd
arturmartins <arturmartinssilva078@gmail.com>
2025-03-31 19:02:08 -0300

    add(LivroRepository): adiciona método para buscar livro por ID de empréstimo

--- src/main/java/br/com/biblioteca/repositories/LivroRepository.java
+++ src/main/java/br/com/biblioteca/repositories/LivroRepository.java
@@ -3,9 +3,14 @@ package br.com.biblioteca.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.biblioteca.entities.LivroEntity;
 
 public interface LivroRepository extends JpaRepository<LivroEntity, Long> {
     List<LivroEntity> findByTitulo(String titulo);
+
+    @Query("SELECT l FROM LivroEntity l WHERE l.id = (SELECT e.livro.id FROM EmprestimoEntity e WHERE e.id = :idEmprestimo)")
+    LivroEntity findLivroByEmprestimoId(@Param("idEmprestimo") Long idEmprestimo);
 }
