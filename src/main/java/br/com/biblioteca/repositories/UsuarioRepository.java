a3a6ef0d96d15fd787f5746aae0091139f8b076e
arturmartins <arturmartinssilva078@gmail.com>
2025-03-31 18:24:13 -0300

    add(UsuarioRepository): adiciona método para buscar usuário por ID de empréstimo

--- src/main/java/br/com/biblioteca/repositories/UsuarioRepository.java
+++ src/main/java/br/com/biblioteca/repositories/UsuarioRepository.java
@@ -1,14 +1,18 @@
 package br.com.biblioteca.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.biblioteca.entities.EnderecoEntity;
 import br.com.biblioteca.entities.UsuarioEntity;
 import java.util.List;
 
-
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
     List<UsuarioEntity> findByNome(String nome);
 
     List<UsuarioEntity> findByEndereco(EnderecoEntity endereco);
+
+    @Query("SELECT u FROM UsuarioEntity u WHERE u.id = (SELECT e.usuario.id FROM EmprestimoEntity e WHERE e.id = :idEmprestimo)")
+    UsuarioEntity findUsuarioByEmprestimoId(@Param("idEmprestimo") Long idEmprestimo);
 }
