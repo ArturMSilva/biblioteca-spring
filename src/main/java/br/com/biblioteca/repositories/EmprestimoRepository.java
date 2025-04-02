64b544b803ca3cfc63ff4e3aee6c9931a6a73f0d
arturmartins <arturmartinssilva078@gmail.com>
2025-04-02 19:21:18 -0300

    feat(EmprestimoRepository): adiciona métodos para verificar status de empréstimos e listar empréstimos vencidos

--- src/main/java/br/com/biblioteca/repositories/EmprestimoRepository.java
+++ src/main/java/br/com/biblioteca/repositories/EmprestimoRepository.java
@@ -2,12 +2,19 @@ package br.com.biblioteca.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.biblioteca.entities.EmprestimoEntity;
+import br.com.biblioteca.entities.EmprestimoEntity.StatusEmprestimo;
 import br.com.biblioteca.entities.LivroEntity;
 import br.com.biblioteca.entities.UsuarioEntity;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
     List<EmprestimoEntity> findByUsuario(UsuarioEntity usuario);
 
     List<EmprestimoEntity> findByLivro(LivroEntity livro);
+
+    boolean existsByLivroAndStatus(LivroEntity livro, StatusEmprestimo emAndamento);
+
+    List<EmprestimoEntity> findByDataDevolucaoBeforeAndStatusNot(LocalDate dataAtual, EmprestimoEntity.StatusEmprestimo status);
 }
