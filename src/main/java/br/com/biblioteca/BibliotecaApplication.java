8ed5c83d16c34bc1de17029934be009b7e9035cd
arturmartins <arturmartinssilva078@gmail.com>
2025-04-20 17:07:58 -0300

    feat(BibliotecaApplication): adiciona endpoint hello para resposta de teste

--- src/main/java/br/com/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/com/biblioteca/BibliotecaApplication.java
@@ -3,11 +3,19 @@ package br.com.biblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RestController
 public class BibliotecaApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaApplication.class, args); //inicia a aplicação	
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world!";
+    }
 }
